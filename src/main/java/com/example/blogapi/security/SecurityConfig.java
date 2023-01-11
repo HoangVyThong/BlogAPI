c9d33ed15b0c8cbd9cdb6abf45d2f5dd57aebb20
HoangVyThong <hoangvythong1@gmail.com>
2023-01-11 13:46:23 +0700

    done UserService

--- src/main/java/com/example/blogapi/security/SecurityConfig.java
+++ src/main/java/com/example/blogapi/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.blogapi.security;
 
+import com.example.blogapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
